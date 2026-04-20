e023cdd6a22df54f67ddbfc12539b1e13d2412ed
paulajuliannamartin-ux <paula.julianna.martin@gmail.com>
2026-04-20 13:45:35 +0200

    Added Post Function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,85 @@
 //Click a button or object to earn points
 // Variables
 let score =0;
-let timeleft =60
+let timeleft =5;
+let gameStarted = false;
+let gameEnded = false;
+let interval = null;
+
 
 //HTML DOM
 const button1 =document.getElementById('button1');
+const button2 =document.getElementById('button2');
 const ScoreDisplay = document.getElementById('ScoreDisplay');
 const timerDisplay = document.getElementById('timerDisplay');
-
+const input1 = document.getElementById('name');
+const label1 = document.getElementById('label1');
 // UI Function
 button1.addEventListener('click', () => {
-  increaseScore();
+ if (!gameEnded) {
+   increaseScore();
+ }
+
+  if (!gameStarted) {
+    startGame();
+  }
+})
+button2.addEventListener('click', () => {
+  submitHighscore();
 })
 
-setInterval(countdown, 1000);
+
+input1.style.display = 'none';
+label1.style.display = 'none';
+button2.display = 'none';
 
 //Function
 function increaseScore() {
   score++;
   ScoreDisplay.innerText = score;
 }
+
 function countdown() {
   timeleft--;
   timerDisplay.innerText = timeleft;
-  //
+
+  if (timeleft <= 0){
+    timerDisplay.innerText = 0;
+    endGame();
+  }
 }
+
+function startGame() {
+ interval = setInterval(countdown, 1000);
+  gameStarted = true;
+}
+
+function endGame() {
+gameEnded = true;
+clearInterval(interval);
+button1.style.display = 'none';
+input1.style.display = 'block';
+label1.style.display = 'block';
+button2.style.display = 'block';
+}
+
+
+
+async function submitHighscore() {
+
+
+  const response = await fetch("https://hooks.zapier.com/hooks/catch/8338993/ujs9jj9/", {
+    method: "POST",
+    body: JSON.stringify({ name: input1.value, score: score }),
+  });
+
+  console.log(response);
+}
+
+
+
+
+
+
+
 
